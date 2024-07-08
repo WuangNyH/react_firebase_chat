690f369d27e1ac0a95e3e2248344f6bd3425eeeb
WuangNyH <135105860+WuangNyH@users.noreply.github.com>
2024-07-08 17:31:58 +0700

    React Notification Library (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import List from './components/list/List'
 import Login from './components/login/Login'
+import Notification from './components/notification/Notification'
 
 const App = () => {
 
-  const user = false
+  const user = true
 
   return (
     <div className='container'>
@@ -17,7 +18,8 @@ const App = () => {
           <Detail/>
         </> 
         ) : (<Login/>)
-      }  
+      }
+      <Notification/>  
       </div>  
   )
 }
