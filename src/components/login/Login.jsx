ab690052d56df78ce9e403439f508fae60661739
WuangNyH <135105860+WuangNyH@users.noreply.github.com>
2024-07-23 23:35:56 +0700

    React FireBase (#14)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../lib/firebase'
 
 const Login = () => {
 
@@ -23,6 +25,23 @@ const Login = () => {
         e.preventDefault()
     }
 
+    // Function to handle the register
+    const handleRegister = async e => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+
+        const {username, email, password} = Object.fromEntries(formData)
+
+        try {
+
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+
+        } catch (err) {
+            console.log(err)
+            toast.error(err.message)
+        }
+    }
+
   return (
     <div className='login'>
         <div className="item">
@@ -36,7 +55,7 @@ const Login = () => {
         <div className="separator"></div>
         <div className="item">
             <h2>Create an Account</h2>
-            <form>
+            <form onSubmit={handleRegister}>
                 <label htmlFor="file">
                     <img src={avatar.url || "./avatar.png"} alt="" />
                     Upload an image</label>
